22c352cca024fbf0f2a8293d47f5747f1a7baf92
lottejackson <lottejackson@yahoo.co.uk>
2014-03-25 21:36:38 +0000

    Styling for task lists, fixed test failure

--- spec/features/task_items/index_spec.rb
+++ spec/features/task_items/index_spec.rb
@@ -3,20 +3,22 @@ require 'spec_helper'
 describe "Viewing task items" do
   let!(:task_list) { TaskList.create(title: "Attendees", description: "Gather list") }
 
-  before do
-    visit "/task_lists"
-    within "#task_list_#{task_list.id}" do
+def visit_task_list(list)
+  visit "/task_lists"
+    within "#task_list_#{list.id}" do
       click_link "List Items"
     end
   end
 
   it "displays the title of the task list" do
+    visit_task_list(task_list)
     within("h1") do
       expect(page).to have_content(task_list.title)
     end
   end
 
   it "displays no items when a task list is empty" do
+    visit_task_list(task_list)
     expect(page.all("ul.task_items li").size).to eq(0)
   end
 end
