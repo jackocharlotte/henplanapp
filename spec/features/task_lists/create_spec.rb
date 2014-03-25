ef54b9381e4172a53d62e11430c4b569f4f0e10a
lottejackson <lottejackson@yahoo.co.uk>
2014-03-25 10:09:01 +0000

    Added validations to task lists and test. Removed views for home page

---
+++ spec/features/task_lists/create_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "Creating task lists" do
+  def create_task_list(options={})
+    options[:title] ||= "My task list"
+    options[:description] ||= "This is my task list."
+
+    visit "/task_lists"
+    click_link "New Task list"
+    expect(page).to have_content("New task_list")
+
+    fill_in "Title", with: options[:title]
+    fill_in "Description", with: options[:description]
+    click_button "Create Task list"
+  end
+
+  it "redirects to the task list index page on success" do
+    create_task_list
+    expect(page).to have_content("My task list")
+  end
+
+  it "displays an error when the task list has no title" do
+    expect(TaskList.count).to eq(0)
+
+    create_task_list title: ""
+
+    expect(page).to have_content("error")
+    expect(TaskList.count).to eq(0)
+
+    visit "/task_lists"
+    expect(page).to_not have_content("This is what I'm doing today.")
+  end
+
+  it "displays an error when the task list has a title less than 3 characters" do
+    expect(TaskList.count).to eq(0)
+
+    create_task_list title: "Hi"
+
+    expect(page).to have_content("error")
+    expect(TaskList.count).to eq(0)
+
+    visit "/task_lists"
+    expect(page).to_not have_content("This is what I'm doing today.")
+  end
+
+end
