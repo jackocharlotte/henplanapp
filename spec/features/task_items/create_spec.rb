a7e71917ccfa15ae78f552ab7904bbe4b45cc71d
lottejackson <lottejackson@yahoo.co.uk>
2014-03-26 15:25:17 +0000

    Another test for creating task items

--- spec/features/task_items/create_spec.rb
+++ spec/features/task_items/create_spec.rb
@@ -1,39 +1,24 @@
 require 'spec_helper'
 
-describe "Adding task items" do
+describe "Viewing task items" do
   let!(:task_list) { TaskList.create(title: "Attendees", description: "Gather list") }
 
-  it "is successful with valid content" do
-    visit_task_list(task_list)
-    click_link "New Task Item"
-    fill_in "Content", with: "Emma"
-    click_button "Save"
-    expect(page).to have_content("Added task list item.")
-    within("ul.task_items") do
-      expect(page).to have_content("Emma")
-    end 
-  end
+def visit_task_list(list)
+  visit "/task_lists"
+    within "#task_list_#{list.id}" do
+      click_link "List Items"
+   	end
+end
 
-  it "displays an error with no content" do
-    visit_task_list(task_list)
-    click_link "New Task Item"
-    fill_in "Content", with: ""
-    click_button "Save"
-    within("div.flash") do
-      expect(page).to have_content("There was a problem adding that task list item.")
-    end
-    expect(page).to have_content("Content can't be blank")
-  end
-
-  it "displays an error with content less than 2 characters long" do
-    visit_task_list(task_list)
-    click_link "New Task Item"
-    fill_in "Content", with: "1"
-    click_button "Save"
-    within("div.flash") do
-      expect(page).to have_content("There was a problem adding that task list item.")
-    end
-    expect(page).to have_content("Content is too short")
+it "is successful with valid content" do
+  	visit_task_list(task_list)
+  	click_link "New Task Item"
+  	fill_in "Content", with: "Emma"
+  	click_button "Save"
+  	expect(page).to have_content("Added task list item.")
+  	within(".task_items") do
+	expect(page).to have_content("Emma")
+  	end 
   end
 
 end
