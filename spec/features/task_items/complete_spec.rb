b7c679d9cbf654f82f00f0fbef248f43a72b81f5
lottejackson <lottejackson@yahoo.co.uk>
2014-03-28 15:19:26 +0000

    Mark task items complete, show completion date or link

--- spec/features/task_items/complete_spec.rb
+++ spec/features/task_items/complete_spec.rb
@@ -14,4 +14,22 @@ describe "Completing task items" do
     expect(task_item.completed_at).to_not be_nil
   end
 
+  context "with completed items" do
+    let!(:completed_task_item) { task_list.task_items.create(content: "Katie", completed_at: 5.minutes.ago) }
+
+      it "shows completed items as complete" do
+        visit_task_list task_list
+        within dom_id_for(completed_task_item) do
+          expect(page).to have_content(completed_task_item.completed_at)
+        end
+      end
+
+
+  it "does not give the option to mark complete" do
+    visit_task_list task_list
+      within dom_id_for(completed_task_item) do
+        expect(page).to_not have_content("Mark Complete")
+      end
+   end
+  end
 end
--- spec/models/task_item_spec.rb
+++ spec/models/task_item_spec.rb
@@ -2,4 +2,18 @@ require 'spec_helper'
 
 describe TaskItem do
   it { should belong_to(:task_list) }
+
+  describe "#completed?" do
+    let(:task_item) { TaskItem.create(content: "Hello") }
+
+    it "is false when completed_is blank" do
+      task_item.completed_at = nil
+      expect(task_item.completed?).to be_false
+    end
+
+    it "returns true when completed_at is not empty" do
+      task_item.completed_at = Time.now
+      expect(task_item.completed?).to be_true
+    end
+  end
 end
