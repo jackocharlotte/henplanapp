33cdb837031cb47a21fdc8cac5345a9f43df9f9e
lottejackson <lottejackson@yahoo.co.uk>
2014-03-28 11:47:25 +0000

    Updated delete spec and updated task list layout

--- spec/features/task_lists/destroy_spec.rb
+++ spec/features/task_lists/destroy_spec.rb
@@ -7,7 +7,7 @@ describe "Deleting task lists" do
     visit "/task_lists"
 
     within "#task_list_#{task_list.id}" do
-      click_link "Destroy"
+      click_link "Delete"
     end
     expect(page).to_not have_content(task_list.title)
     expect(TaskList.count).to eq(0)
