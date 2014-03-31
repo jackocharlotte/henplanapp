4a4e0679d79cca6cdd4532567e14c8636e8615c5
lottejackson <lottejackson@yahoo.co.uk>
2014-03-31 14:12:05 +0100

    Added created date and user id to the task lists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140328175357) do
+ActiveRecord::Schema.define(version: 20140331125331) do
 
   create_table "homes", force: true do |t|
     t.datetime "created_at"
@@ -33,7 +33,10 @@ ActiveRecord::Schema.define(version: 20140328175357) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "task_lists", ["user_id"], name: "index_task_lists_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "username"
