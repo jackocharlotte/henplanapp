bd0f725ca4cff2c6d8f8baab73901334d632d4c0
lottejackson <lottejackson@yahoo.co.uk>
2014-03-31 12:28:22 +0100

    Styling update to dashboard and added webfornts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+  has_many :task_lists
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
