280309f65f4fe7e7e25e5010d4cab37c2c95a5b6
lottejackson <lottejackson@yahoo.co.uk>
2014-04-08 07:41:26 +0100

    Buget section finished

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -28,7 +28,7 @@ class BudgetsController < ApplicationController
 
     respond_to do |format|
       if @budget.save
-        format.html { redirect_to @budget, notice: 'Budget was successfully created.' }
+        format.html { redirect_to budgets_url, notice: 'Budget item was successfully added.' }
         format.json { render action: 'show', status: :created, location: @budget }
       else
         format.html { render action: 'new' }
@@ -42,7 +42,7 @@ class BudgetsController < ApplicationController
   def update
     respond_to do |format|
       if @budget.update(budget_params)
-        format.html { redirect_to @budget, notice: 'Budget was successfully updated.' }
+        format.html { redirect_to budgets_url, notice: 'Budget was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
