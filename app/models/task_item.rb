ef449e72d38de47051ee01395fc7de2331906533
lottejackson <lottejackson@yahoo.co.uk>
2014-05-06 08:55:03 +0100

    Added reference to task completion code

--- app/models/task_item.rb
+++ app/models/task_item.rb
@@ -3,6 +3,7 @@ class TaskItem < ActiveRecord::Base
 
   validates :content, presence: true
 
+  #Completing tasks functionality from Treehouse tutorial - http://teamtreehouse.com/library/build-a-todo-list-application-with-rails-4/build-a-todo-list-application-with-rails-4/marking-todo-items-complete
   scope :complete, -> { where("completed_at is not null") }
   scope :incomplete, -> { where(completed_at: nil) }
   					  
